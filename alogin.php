4bd82baa40f59f52d47254c69beb0a95db62324b
MAMMU_OT7 <109816587+Pratheepa-chintha@users.noreply.github.com>
2022-09-25 21:58:21 +0530

    Add files via upload

--- alogin.php
+++ alogin.php
@@ -1,13 +1,8 @@
 <?php
-//$servername="localhost";
-//$database="emsdb";
-//$username="username";
-//$password="password";
 $username=$_REQUEST['t1'];
 $password=$_REQUEST['t2'];
 $con=mysqli_connect("localhost:3306","root"," ","emsdb");
-//$conn=mysqli_connect($servername,$username,$password,$database);
-$rows=mysqli_query($con,"select count(*) from credentials where username='$username' and password='$password'");
+$rows=mysqli_query($con,"select count(*) from alogin where username='$username' and password='$password'");
 $row=mysqli_fetch_row($rows);
 $n=$row[0];
 if($n==1)
--- elogin.php
+++ elogin.php
@@ -1,17 +1,12 @@
 <?php
-//$servername="localhost";
-//$database="database";
-//$username="username";
-//$password="password";
-$username=$_REQUEST['t3'];
-$password=$_REQUEST['t4'];
+$username=$_REQUEST['t1'];
+$password=$_REQUEST['t2'];
 $con=mysqli_connect("localhost:3306","root"," ","emsdb");
-//$conn=mysqli_connect($servername,$username,$password,$database);
-$rows=mysqli_query($con,"select count(*) from emsdb where username='$username' and password='$password'");
+$rows=mysqli_query($con,"select count(*) from elogin where username='$username' and password='$password'");
 $row=mysqli_fetch_row($rows);
 $n=$row[0];
 if($n==1)
     header("location:empentry.html"); 
 else
-    echo"<br><br><center><h1 style='color:voilet'>Invalid Useername or password</h1>";
+    echo"<br><br><center><h1 style='color:voilet'>Invalid Username or password</h1>";
 ?>
